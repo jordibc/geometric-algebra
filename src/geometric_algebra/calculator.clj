07cf97184c806160a0c2d3adb6a8a5090952f430
Jordi <jordi.burguet.castell@gmail.com>
2025-12-26 02:48:28 +0100

    Allow calculator to work with infix expressions or s-expressions.

--- src/geometric_algebra/calculator.clj
+++ src/geometric_algebra/calculator.clj
@@ -17,33 +17,42 @@
       (or sig-name (ga/vector->signature [p q r] start)))
     (catch Exception e (do (println "Error: bad signature" args) {}))))
 
+(defn- op-expand [s op] ; put spaces around appearances of the given operator
+  (str/replace s op (str " " op " ")))
+
+(defn- ops-expand [s] ; put spaces around all operators
+  (reduce op-expand s (keys ga/operators)))
+
+(defn- line->expr [line infix?]
+  (if-not infix?
+    (edn/read-string line)
+    (ga/infix->sexpr (edn/read-string (str "(" (ops-expand line) ")")))))
+
 (defn calc
-  "REPL to get infix GA expressions and show their values."
-  [signature]
-  (let [basis (rest (ga/basis signature)) ; basis multivectors
-        funcs {'rev ga/rev, 'invol ga/invol, 'inv ga/inv, 'dual ga/dual,
-               'grade ga/grade, 'pow ga/pow, 'norm ga/norm, 'proj ga/proj,
-               'exp ga/exp}
-        env (flatten (concat (for [e basis] [(symbol (str e)) e])
-                             (for [[op f] ga/operators] [(symbol op) f])
-                             funcs))
-        op-expand (fn [s op] (str/replace s op (str " " op " ")))
-        ops-expand (fn [s] (reduce op-expand s (keys ga/operators)))]
-    (println "Basis multivectors:" (str/join " " basis)) ; "e1, e2, e12"
-    (let [f (fn [[i sig]] (str "e" i "e" i "=" sig))] ; like "e1e1=-1"
-      (println "Signature:" (str/join ", " (map f signature))))
-    (println "Operators:" (str/join " " (keys ga/operators)))
-    (loop []
-      (print "> ")
-      (flush)
-      (let [line (read-line)] ; user input
-        (when-not (nil? line) ; so ctrl+d exits
-          (try
-            (let [expr-str (str "(" (ops-expand line) ")")
-                  expr (ga/infix->sexpr (edn/read-string expr-str))
-                  val (eval-with-env expr env)]
-              (when-not (nil? val) ; so it doesn't print "nil" if empty
-                (println val))) ; result
-            (catch Exception e (println (.getMessage e)))
-            (catch AssertionError e (println (.getMessage e))))
-          (recur))))))
+  "REPL to get GA expressions and show their values."
+  ([signature] (calc signature true))
+  ([signature infix?]
+   (let [basis (rest (ga/basis signature)) ; basis multivectors
+         funcs {'rev ga/rev, 'invol ga/invol, 'inv ga/inv, 'dual ga/dual,
+                'grade ga/grade, 'pow ga/pow, 'norm ga/norm, 'proj ga/proj,
+                'exp ga/exp}
+         env (flatten (concat (for [e basis] [(symbol (str e)) e])
+                              (for [[op f] ga/operators] [(symbol op) f])
+                              funcs))]
+     (println "Basis multivectors:" (str/join " " basis)) ; "e1, e2, e12"
+     (let [f (fn [[i sig]] (str "e" i "e" i "=" sig))] ; like "e1e1=-1"
+       (println "Signature:" (str/join ", " (map f signature))))
+     (println "Operators:" (str/join " " (keys ga/operators)))
+     (loop []
+       (print "> ")
+       (flush)
+       (let [line (read-line)] ; user input
+         (when-not (nil? line) ; so ctrl+d exits
+           (try
+             (let [expr (line->expr line infix?)
+                   val (eval-with-env expr env)]
+               (when-not (nil? val) ; so it doesn't print "nil" if empty
+                 (println val))) ; result
+             (catch Exception e (println (.getMessage e)))
+             (catch AssertionError e (println (.getMessage e))))
+           (recur)))))))
