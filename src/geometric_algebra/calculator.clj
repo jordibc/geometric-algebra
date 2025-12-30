a28a6d552bce87dc8f6fb301fdf15a7c474d8561
Jordi <jordi.burguet.castell@gmail.com>
2025-12-31 00:52:21 +0100

    calculator.clj: Allow the use of a "help" command, and define variables.
    Things like:
    
      help proj
    
    will show a message with information about the "proj" function.
    
    And things like:
    
      a = e0 + 2*e1
    
    will define a new variable "a" with the given value, that can be later
    used in any expression.
    
    This starts being quite useful as a calculator.

--- src/geometric_algebra/calculator.clj
+++ src/geometric_algebra/calculator.clj
@@ -4,7 +4,8 @@
             [clojure.string :as str]))
 
 (defn- eval-with-env [code env]
-  (eval `(let ~(vec env) ~code)))
+  (let [bindings (vec (mapcat vec env))] ; map -> flat vector (for bindings)
+    (eval `(let ~bindings ~code))))
 
 (defn- args->signature [args]
   (try
@@ -33,13 +34,27 @@
         (edn/read-string) ; read (parse) it
         (ga/infix->sexpr)))) ; and transform from infix to sexp
 
-(defn info [basis signature functions]
+(defn- info [basis signature functions]
   (str
    "Basis multivectors: " (str/join " " basis) "\n" ; "e1, e2, e12"
    "Signature: " (let [f (fn [[i sig]] (str "e" i "e" i "=" sig))] ; "e1e1=-1"
                    (str/join " " (map f signature))) "\n"
    "Functions: " (str/join " " (keys functions)) "\n"
    "Operators: " (str/join " " (keys ga/operators))))
+
+(defn- show-help [line env]
+  (let [func-name (str/trim (second (str/split line #" ")))
+        var (or (resolve (symbol func-name)) ; for "+" and so on
+                (resolve (symbol (str "geometric-algebra.core/" func-name))))]
+    (println (:doc (meta var)))))
+
+(defn- line->val [line env infix?]
+  (try
+    (let [expr (line->expr line infix?)
+          val (eval-with-env expr env)]
+      val)
+    (catch Exception e (println (.getMessage e)))
+    (catch AssertionError e (println (.getMessage e)))))
 
 (defn calc
   "REPL to get GA expressions and show their values."
@@ -49,20 +64,24 @@
          funcs {'rev ga/rev, 'invol ga/invol, 'inv ga/inv, 'dual ga/dual,
                 'grade ga/grade, 'pow ga/pow, 'norm ga/norm, 'exp ga/exp,
                 'proj ga/proj, 'rej ga/rej}
-         env (flatten (concat (for [e basis] [(symbol (str e)) e])
-                              (for [[op f] ga/operators] [(symbol op) f])
-                              funcs))]
+         env0 (into {} (concat (for [e basis] [(symbol (str e)) e])
+                               (for [[op f] ga/operators] [(symbol op) f])
+                               funcs))]
      (println (info basis signature funcs))
-     (loop []
+     (loop [env env0]
        (print "> ")
        (flush)
        (let [line (read-line)] ; user input
          (when-not (nil? line) ; so ctrl+d exits
-           (try
-             (let [expr (line->expr line infix?)
-                   val (eval-with-env expr env)]
-               (when-not (nil? val) ; so it doesn't print "nil" if empty
-                 (println val))) ; result
-             (catch Exception e (println (.getMessage e)))
-             (catch AssertionError e (println (.getMessage e))))
-           (recur)))))))
+           (cond
+             (str/starts-with? line "help ") (do
+                                               (show-help line env)
+                                               (recur env))
+             (str/includes? line "=") (let [[var line] (str/split line #"\W*=")
+                                            val (line->val line env infix?)
+                                            env-new (assoc env (symbol var) val)]
+                                        (recur env-new)) ; add var to environment
+             :else (let [val (line->val line env infix?)]
+                     (when-not (nil? val) ; so it doesn't print "nil" if empty
+                       (println val)) ; result
+                     (recur env)))))))))
