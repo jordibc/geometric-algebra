f235fc0a83c56e79072c39e798e5a1505297411b
Jordi <jordi.burguet.castell@gmail.com>
2025-12-31 18:42:19 +0100

    cosmetics

--- test/geometric_algebra/calculator_test.clj
+++ test/geometric_algebra/calculator_test.clj
@@ -13,26 +13,27 @@
 
 (deftest calc-test
   (let [sig (calc/args->signature ["sta"])
-        calc #(with-out-str (with-in-str % (calc/calc sig)))]
+        calc #(with-out-str (with-in-str % (calc/calc sig)))
+        result? (fn [x y] (is (= (calc x) (output y))))]
     (testing "Calculator usage (infix)"
       (testing "with simple value"
-        (is (= (calc "3") (output "3"))))
+        (result? "3" "3"))
       (testing "with standard operations"
-        (is (= (calc "1 + 2") (output "3")))
-        (is (= (calc "4 * 5") (output "20")))
-        (is (= (calc "1 / 5") (output "1/5"))))
+        (result? "1 + 2" "3")
+        (result? "4 * 5" "20")
+        (result? "1 / 5" "1/5"))
       (testing "with multivectors"
-        (is (= (calc "2 * (7 e3 + 6 e12 + 4 e23)")
-               (output "14 e3 + 12 e12 + 8 e23")))
-        (is (= (calc "pow(7 e3 + 6 e12 + 4 e23, 3)")
-               (output "-336 e1 + -1211 e3 + -1194 e12 + -404 e23"))))
+        (result? "2 * (7 e3 + 6 e12 + 4 e23)"
+                 "14 e3 + 12 e12 + 8 e23")
+        (result? "pow(7 e3 + 6 e12 + 4 e23, 3)"
+                 "-336 e1 + -1211 e3 + -1194 e12 + -404 e23"))
       (testing "requesting help"
-        (is (= (calc "help") (output "Usage: help <function>")))
-        (is (= (calc "help proj")
-               (output (str "  proj ([a b])\n"
-                            "  Return P_b(a), the projection of multivector `a` on `b`."))))
-        (is (= (calc "help *")
-               (output (str "  * ([] [a] [a b] [a b & more])\n"
-                            "  Return a * b, the geometric product of multivectors `a` and `b`.")))))
+        (result? "help" "Usage: help <function>")
+        (result? "help proj"
+                 (str "  proj ([a b])\n"
+                      "  Return P_b(a), the projection of multivector `a` on `b`."))
+        (result? "help *"
+                 (str "  * ([] [a] [a b] [a b & more])\n"
+                      "  Return a * b, the geometric product of multivectors `a` and `b`.")))
       (testing "assigning variables"
-        (is (= (calc "a = e2 + e1\na") (output "> e1 + e2")))))))
+        (result? "a = e2 + e1\na" "> e1 + e2")))))
