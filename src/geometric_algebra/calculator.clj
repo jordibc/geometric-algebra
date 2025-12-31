21e4417d25f5a2bdff4f4429e668c20814c2a645
Jordi <jordi.burguet.castell@gmail.com>
2025-12-31 16:49:28 +0100

    Many small changes: help shows all documentation, better assignments, etc.
    With the environment map mapping to vars instead of functions, we can
    easily access now the documentation for every function and operator we
    use.
    
    Also, we can exit the calculator by writing "exit" or "quit".
    
    The last result is automatically kept in a variable called "ans" (as
    in, the last answer we got, like in octave).
    
    And in general quite a bit of cleanup and reordering for clarity.

--- src/geometric_algebra/calculator.clj
+++ src/geometric_algebra/calculator.clj
@@ -3,11 +3,7 @@
             [clojure.edn :as edn]
             [clojure.string :as str]))
 
-(defn- eval-with-env [code env]
-  (let [bindings (vec (mapcat vec env))] ; map -> flat vector (for bindings)
-    (eval `(let ~bindings ~code))))
-
-(defn- args->signature [args]
+(defn args->signature [args]
   (try
     (let [[a1 a2 a3 a4] args
           sig-name (ga/name->signature (or a1 "")) ; signature given as a name
@@ -18,23 +14,12 @@
       (or sig-name (ga/vector->signature [p q r] start)))
     (catch Exception e (do (println "Error: bad signature" args) {}))))
 
-(defn- op-expand [s op] ; put spaces around appearances of the given operator
-  (str/replace s op (str " " op " ")))
+(def ^:private functions
+  {'rev #'ga/rev, 'invol #'ga/invol, 'inv #'ga/inv, 'dual #'ga/dual,
+   'grade #'ga/grade, 'pow #'ga/pow, 'norm #'ga/norm, 'exp #'ga/exp,
+   'proj #'ga/proj, 'rej #'ga/rej})
 
-(defn- ops-expand [s] ; put spaces around all operators
-  (reduce op-expand s (keys ga/operators)))
-
-(defn- line->expr [line infix?]
-  (if-not infix?
-    (edn/read-string line) ; easy case, read the s-expression and return it
-    (-> line ; less easy case, we have an infix expression
-        (ops-expand) ; put spaces around operators
-        (str/replace #"," ") (") ; function arguments as sexps
-        (#(str "(" % ")")) ; make the full line a single expression
-        (edn/read-string) ; read (parse) it
-        (ga/infix->sexpr)))) ; and transform from infix to sexp
-
-(defn- info [basis signature functions]
+(defn- info [basis signature]
   (str
    "Basis multivectors: " (str/join " " basis) "\n" ; "e1, e2, e12"
    "Signature: " (let [f (fn [[i sig]] (str "e" i "e" i "=" sig))] ; "e1e1=-1"
@@ -42,46 +27,76 @@
    "Functions: " (str/join " " (keys functions)) "\n"
    "Operators: " (str/join " " (keys ga/operators))))
 
-(defn- show-help [line env]
-  (let [func-name (str/trim (second (str/split line #" ")))
-        var (or (resolve (symbol func-name)) ; for "+" and so on
-                (resolve (symbol (str "geometric-algebra.core/" func-name))))]
-    (println (:doc (meta var)))))
+(defn- help [text env]
+  (let [[_ func-name] (str/split text #"\s+")]
+    (if (nil? func-name)
+      "Usage: help <function>"
+      (let [meta (meta (get env (symbol func-name)))]
+        (if (nil? meta)
+          (str "Cannot find documentation for function: " func-name)
+          (str "  " func-name " " (:arglists meta) "\n"
+               "  " (:doc meta)))))))
 
-(defn- line->val [line env infix?]
+(defn- op-expand [s op] ; put spaces around appearances of the given operator
+  (str/replace s op (str " " op " ")))
+
+(defn- ops-expand [s] ; put spaces around all operators
+  (reduce op-expand s (keys ga/operators)))
+
+(defn- text->expr [text infix?]
+  (if-not infix?
+    (edn/read-string text) ; easy case, read the s-expression and return it
+    (-> text ; less easy case, we have an infix expression
+        (ops-expand) ; put spaces around operators
+        (str/replace #"," ") (") ; function arguments as sexps
+        (#(str "(" % ")")) ; make the full text a single expression
+        (edn/read-string) ; read (parse) it
+        (ga/infix->sexpr)))) ; and transform from infix to sexp
+
+(defn- eval-with-env [expr env]
+  (let [bindings (vec (mapcat vec env))] ; map -> flat vector (for bindings)
+    (eval `(let ~bindings ~expr))))
+
+(defn- text->val
+  "Return value of evaluating text with the given environment (nil on error)."
+  [text env infix?]
   (try
-    (let [expr (line->expr line infix?)
+    (let [expr (text->expr text infix?)
           val (eval-with-env expr env)]
       val)
     (catch Exception e (println (.getMessage e)))
     (catch AssertionError e (println (.getMessage e)))))
+
+(defn- entry-type [text]
+  (let [parts (str/split text #"\s")]
+    (cond
+      (= "help" (first parts)) :help ; help request
+      (= "=" (second parts))   :assign ; assign to variable
+      :else                    :eval))) ; evaluation
 
 (defn calc
   "REPL to get GA expressions and show their values."
   ([signature] (calc signature true))
   ([signature infix?]
    (let [basis (rest (ga/basis signature)) ; basis multivectors
-         funcs {'rev ga/rev, 'invol ga/invol, 'inv ga/inv, 'dual ga/dual,
-                'grade ga/grade, 'pow ga/pow, 'norm ga/norm, 'exp ga/exp,
-                'proj ga/proj, 'rej ga/rej}
          env0 (into {} (concat (for [e basis] [(symbol (str e)) e])
                                (for [[op f] ga/operators] [(symbol op) f])
-                               funcs))]
-     (println (info basis signature funcs))
+                               functions))]
+     (println (info basis signature))
      (loop [env env0]
        (print "> ")
        (flush)
        (let [line (read-line)] ; user input
-         (when-not (nil? line) ; so ctrl+d exits
-           (cond
-             (str/starts-with? line "help ") (do
-                                               (show-help line env)
-                                               (recur env))
-             (str/includes? line "=") (let [[var line] (str/split line #"\W*=")
-                                            val (line->val line env infix?)
-                                            env-new (assoc env (symbol var) val)]
-                                        (recur env-new)) ; add var to environment
-             :else (let [val (line->val line env infix?)]
-                     (when-not (nil? val) ; so it doesn't print "nil" if empty
-                       (println val)) ; result
-                     (recur env)))))))))
+         (when-not (or (nil? line) (= "exit" line) (= "quit" line))
+           (let [text (str/trim line)]
+             (case (entry-type text)
+               :help   (do
+                         (println (help text env))
+                         (recur env))
+               :assign (let [[var text] (str/split text #"\s*=")
+                             val (text->val text env infix?)]
+                         (recur (assoc env (symbol var) val))) ; new environment
+               :eval   (let [val (text->val text env infix?)]
+                         (when-not (nil? val) ; don't print "nil" on empty line
+                           (println val)) ; evaluation output
+                         (recur (assoc env 'ans val))))))))))) ; keep answer
