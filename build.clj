3af2114ea32e603bb191054ab23a9fc9d491f6a6
Jordi <jordi.burguet.castell@gmail.com>
2026-01-17 08:18:15 +0100

    Bump version to 0.9.9.

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'net.clojars.jordibc/geometric-algebra)
-(def version "0.9.8")
+(def version "0.9.9")
 (def class-dir "target/classes")
 
 (defn test "Run all the tests." [opts]
